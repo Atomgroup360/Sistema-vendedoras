ee83469df0f84650a3379b354db0a2397332bb3a
Atomgroup360 <julian.1818@gmail.com>
2026-04-25 22:20:36 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../App';  // ← importante: App está en la raíz
 import { AuthProvider } from './context/AuthContext';
-import './index.css'; // si tienes este archivo, sino elimina esta línea
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
